3b58e465d6fef9655f924e04b187395803baf0f7
RoaRos2808 <roanrosema@gmail.com>
2023-11-02 18:18:08 +0100

    Some content for the timeline

--- src/pages/TimelinePage/TimelinePage.tsx
+++ src/pages/TimelinePage/TimelinePage.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './TimelinePage.css';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
+import ConnectingAirportsIcon from '@mui/icons-material/ConnectingAirports';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 // interface HomePageProps {
 //     path: string;
@@ -18,6 +20,7 @@ const TimelinePage: React.FC = () => {
             contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
             date="26 november 2022"
             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            icon={<ConnectingAirportsIcon />}
           >
             <h3 className="vertical-timeline-element-title">De allereerste ontmoeting</h3>
             <h4 className="vertical-timeline-element-subtitle">Sofia, Bulgarije</h4>
@@ -25,6 +28,37 @@ const TimelinePage: React.FC = () => {
               Vet leukkkkk
             </p>
           </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+            date="27 november 2022"
+            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            icon={<FavoriteBorderIcon />}
+          >
+            <h3 className="vertical-timeline-element-title">Zoenennn</h3>
+            <h4 className="vertical-timeline-element-subtitle">Sofia, Bulgarije</h4>
+            <p>
+              Vet leukkkkk
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            contentStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+            contentArrowStyle={{ borderRight: '7px solid  rgb(233, 30, 99)' }}
+            date="April 2013"
+            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+            visible
+          >
+            <h3 className="vertical-timeline-element-title">
+              [VISIBLE BY DEFAULT] Content Marketing for Web, Mobile and Social Media
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+            <p>Strategy, Social Media</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+          />
         </VerticalTimeline>
       </div>
     </div>
